8816475642387cc04539a370a0d08ba46830ae99
Jesselyn Jackson <Jesselyn.Jackson@gmail.com>
2022-08-17 15:21:49 -0400

    Added additional section content to About page

--- src/constants/labels.tsx
+++ src/constants/labels.tsx
@@ -18,5 +18,15 @@ export const ABOUT_SECTION_LABELS = {
     PROFESSIONS: "Professions",
     RADIO: "Radio Co-Host",
     INTERVIEWS: "Interviews",
-    STREAMING: "Kiss 104.1 fm w/Mitch Faulkner"
+    STREAMING: "Kiss 104.1 fm w/Mitch Faulkner",
+    SUMMARY_VOCALIST_HEADER: "Vocal Stylist",
+    SUMMARY_VOCALIST_TEXT: "LET THERE BE LOVE / GROWN & SEXY LULLABIES / WOMANFLOWER",
+    SUMMARY_AUTHOR_HEADER: "Author",
+    SUMMARY_AUTHOR_TEXT: "THE CAPRICIOUS & ANECDOTAL SOJOURN OF A WOMAN WHO SOMETIMES PROCESSES INFORMATION IN A PECULIAR FASHION",
+    SUMMARY_FILMMAKER_HEADER: "Filmmaker",
+    SUMMARY_FILMMAKER_TEXT: "IT'S NEVER TOO LATE / THE HEALING HOUSE / AND COMING SOON... HEAVY HITTERS",
+    ABOUT_POET_ET_ALL_HEADER: "Poet/Producer/Author/Songwriter/Vocal Stylist/Filmmaker",
+    ABOUT_POET_ET_ALL_TEXT1: "Dee Robinson considers herself, first and foremost, a poet, which is the cradle of her creativity. She became a writer-for-hire when she founded Dee Robinson the Right Writer Professional Writing Service. She spent over 20 years as a speech writer, ghostwriter, editor, consultant, and ‘idea-ist’ for clients. After helping many others write books, she’s written one in her own name titled ",
+    ABOUT_POET_ET_ALL_TEXT2: "The Capricious & Anecdotal Sojourn of a Woman who Sometimes Processes Information in a Peculiar Fashion",
+    ABOUT_POET_ET_ALL_TEXT3: ".  In 1998, Dee established Mentaltainment, a production company with an emphasis on enlightening and uplifting live entertainment. The concept behind Mentaltainment is to provide “information disguised as entertainment.”"
 }
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,19 +1,57 @@
 import React, { FC } from 'react';
 import { ABOUT_SECTION_LABELS } from '../../constants/labels';
 import './About.scss';
+import vocalStylistImg from '../../assets/images/about/vocalStylist_lowRes.jpg';
+import authorImg from '../../assets/images/about/author.jpg';
+import filmMakerImg from '../../assets/images/about/filmMaker.jpg';
 
 interface AboutProps {}
 
 const About: FC<AboutProps> = () => (
   <div className="About" data-testid="About">
     <div className='section'>
+      <div className='section-content'>
+        <div className='summary'>
+          <span className='summary-content'>
+              <img className='roundedImg' src={vocalStylistImg} alt="vocalStylist" />
+              <div className='details'>
+                <div className='detail-header'>{ABOUT_SECTION_LABELS.SUMMARY_VOCALIST_HEADER}</div>
+                <p className='detail-text'>{ABOUT_SECTION_LABELS.SUMMARY_VOCALIST_HEADER}</p>
+              </div>
+          </span>
+          <span className='summary-content'>
+            <img className='roundedImg' src={authorImg} alt="vocalStylist" />
+            <div className='details'>
+                <div className='detail-header'>{ABOUT_SECTION_LABELS.SUMMARY_AUTHOR_HEADER}</div>
+                <p className='detail-text'>{ABOUT_SECTION_LABELS.SUMMARY_AUTHOR_TEXT}</p>
+              </div>
+          </span>
+          <span className='summary-content'>
+            <img className='roundedImg' src={filmMakerImg} alt="vocalStylist" />
+            <div className='details'>
+                <div className='detail-header'>{ABOUT_SECTION_LABELS.SUMMARY_FILMMAKER_HEADER}</div>
+                <p className='detail-text'>{ABOUT_SECTION_LABELS.SUMMARY_FILMMAKER_TEXT}</p>
+              </div>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div className='section offColor'>
       <div className='section-header'>
         <span>{ABOUT_SECTION_LABELS.ABOUT}</span>
       </div>
-    </div>
-    <div className='section'>
-      <div className='section-header'>
-        <span>{ABOUT_SECTION_LABELS.PROFESSIONS}</span>
+      <div className='summary-content'>
+        <div className='picSide'>
+          <img className='boxedImg' src={authorImg} alt="vocalStylist" />
+        </div>
+        <div className='textSide'>
+          <h4>{ABOUT_SECTION_LABELS.ABOUT_POET_ET_ALL_HEADER}</h4>
+          <p>
+            {ABOUT_SECTION_LABELS.ABOUT_POET_ET_ALL_TEXT1}
+            <span className='i'>{ABOUT_SECTION_LABELS.ABOUT_POET_ET_ALL_TEXT2}</span>
+            {ABOUT_SECTION_LABELS.ABOUT_POET_ET_ALL_TEXT3}
+          </p>
+        </div>
       </div>
     </div>
     <div className='section'>
