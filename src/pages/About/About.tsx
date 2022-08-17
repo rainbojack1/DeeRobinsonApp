c6f52408a96df3f7531373cba1174d6b0058afe3
Jesselyn Jackson <Jesselyn.Jackson@gmail.com>
2022-08-17 11:06:33 -0400

    Created sections & styled headers for About Page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,7 +5,31 @@ interface AboutProps {}
 
 const About: FC<AboutProps> = () => (
   <div className="About" data-testid="About">
-    About Component
+    <div className='section'>
+      <div className='section-header'>
+        <span>About Dee</span>
+      </div>
+    </div>
+    <div className='section'>
+      <div className='section-header'>
+        <span>Professions</span>
+      </div>
+    </div>
+    <div className='section'>
+      <div className='section-header'>
+        <span>Radio Co-Host</span>
+      </div>
+    </div>
+    <div className='section'>
+      <div className='section-header'>
+        <span>Interviews</span>
+      </div>
+    </div>
+    <div className='section'>
+      <div className='section-header'>
+        <span>Kiss 104.1 fm w/Mitch Faulkner</span>
+      </div>
+    </div>
   </div>
 );
 
